1e705e863a69cdc924174b84f2023e2879b5d36f
RasulRustamli <rasular@code.edu.az>
2022-09-06 23:20:47 +0400

    Add project files.

---
+++ CustomSerializer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+
+namespace CustomSerializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>
+            {
+                new Product
+                {
+                    Name = "ProductTest",
+                    Categories = new List<Category>
+                    {
+                        new Category
+                        {
+                          Name = "CategoryTest"
+                        }
+                    }
+                },
+                new Product
+                {
+                    Name = "Asgar",
+                },
+            };
+
+          
+
+            var customSerialize = CustomJsonSerializer.Serialize(list);
+            Console.WriteLine(customSerialize);
+
+            var defaultSerialize = JsonSerializer.Serialize(list);
+            Console.WriteLine(defaultSerialize);
+
+        }
+    }
+}
